c3535c7380cb6c51e4adbad2e1a763878a8d925b
Nicolas Peixoto <nicolaspeixoto93+github@gmail.com>
2025-08-25 19:39:25 -0300

    adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en-us" className={libreFranklin.className}>
       <body>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BRT88BC1EZ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BRT88BC1EZ');
+            `,
+          }}
+        />
         <main className="max-w-5xl px-10 mx-auto">
           {children}
         </main>
